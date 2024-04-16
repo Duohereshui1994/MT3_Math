6789f72850095785e48d5efd446e9f846cfeff17
GC1A_04_ゴ_ウ <147117585+Duohereshui1994@users.noreply.github.com>
2024-04-16 11:31:11 +0900

    MT3_00_04_確認課題

--- MT3_Math/main.cpp
+++ MT3_Math/main.cpp
@@ -55,7 +55,7 @@ Matrix4x4 MakeRotateZMatrix(float radian) {
 	return result;
 }
 
-static Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
 {
 	Matrix4x4 result = {};
 
@@ -74,7 +74,7 @@ static Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
 static const int kColumnWidth = 60;
 
 static const int kRowHeight = 20;
-static void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
 
 	Novice::ScreenPrintf(x, y - kRowHeight, "%s", label);
 
