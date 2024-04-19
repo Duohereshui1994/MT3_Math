e89c3d108ebe4c9ec6fa56145b7491cec0bcaee6
GC1A_04_ゴ_ウ <147117585+Duohereshui1994@users.noreply.github.com>
2024-04-19 23:39:36 +0900

    0101初具人形

--- MT3_Math/main.cpp
+++ MT3_Math/main.cpp
@@ -16,8 +16,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
-	int kWindowWidth = 1280;
-	int kWindowHeight = 720;
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -26,14 +26,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 v1{ 1.2f,-3.9f,2.5f };
 	Vector3 v2{ 2.8f,0.4f,-1.3f };
 
-	Vector3 rotate{0.0f,0.0f,0.0f};
-	Vector3 translate{ 0.0f,0.0f,0.0f };
-	Vector3 cameraPosition{ 640.0f,360.0f ,0.0f };
+	Vector3 rotate{};
+	Vector3 translate{};
+	Vector3 cameraPosition{ 0.0f,0.5f ,-10.0f };
 
 	Vector3 kLocalVertices[3]{};
-	kLocalVertices[0] = { 100.0f,100.0f,50.0f };
-	kLocalVertices[1] = { 50.0f,200.0f,50.0f };
-	kLocalVertices[2] = { 150.0f,200.0f,50.0f };
+	kLocalVertices[0] = { 0.5f,1.0f,0.0f };
+	kLocalVertices[1] = { 0.0f,0.0f,0.0f };
+	kLocalVertices[2] = { 1.0f,0.0f,0.0f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -48,17 +48,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 		Vector3 cross = Cross(v1, v2);
-		if (keys[DIK_D]) {
-			translate.x += 5;
-		}
-		if (keys[DIK_A]) {
-			translate.x -= 5;
+
+		if (keys[DIK_RIGHT]) {
+			kLocalVertices[0].x += 0.1f;
+			kLocalVertices[1].x += 0.1f;
+			kLocalVertices[2].x += 0.1f;
 		}
 		if (keys[DIK_UP]) {
-			translate.y += 5;
+			/*kLocalVertices[0].z += 0.1f;
+			kLocalVertices[1].z += 0.1f;
+			kLocalVertices[2].z += 0.1f;*/
+			cameraPosition.z += 0.1f;
 		}
 		if (keys[DIK_DOWN]) {
-			translate.y -= 5;
+			/*kLocalVertices[0].z -= 0.1f;
+			kLocalVertices[1].z -= 0.1f;
+			kLocalVertices[2].z -= 0.1f;*/
+			cameraPosition.z -= 0.1f;
 		}
 
 		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
@@ -66,7 +72,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
 		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		//Matrix4x4 worldViewProjectionMatrix = Multiply(Multiply(worldMatrix, viewMatrix), projectionMatrix);
 		Matrix4x4 viewPortMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
 		Vector3 screenVertices[3];
 		for (uint32_t i = 0; i < 3; ++i) {
