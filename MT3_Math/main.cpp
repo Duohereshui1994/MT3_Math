347479c8314d6d7f1d4f7b248c3fd68c9ef171af
GC1A_04_ゴ_ウ <147117585+Duohereshui1994@users.noreply.github.com>
2024-04-23 12:58:04 +0900

    做不对

--- MT3_Math/main.cpp
+++ MT3_Math/main.cpp
@@ -8,26 +8,44 @@ void DrawGrid(const Matrix4x4& viewProjectionMatrix, Matrix4x4& viewportMatrix) 
 	const float kGridHalfWidth = 2.0f;//Gridの半分の幅
 	const uint32_t kSubdivision = 10;//分割数
 	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);//一つ分の長さ
-	Vector3 posHorizontalStart[kSubdivision + 1];
-	Vector3 posHorizontalEnd[kSubdivision + 1];
-	Vector3 posHorizontalStartScreen[kSubdivision + 1];
-	Vector3 posHorizontalEndScreen[kSubdivision + 1];
 
 	Matrix4x4 worldViewProjectionMatrix = viewProjectionMatrix;
 	Matrix4x4 viewPortMatrix = viewportMatrix;
+	float xStart[kSubdivision];
+	float xEnd[kSubdivision];
+	float zStart[kSubdivision];
+	float zEnd[kSubdivision];
+	Vector3 start[kSubdivision];
+	Vector3 end[kSubdivision];
+	Vector3 startPointViewport[kSubdivision];
+	Vector3 endPointViewport[kSubdivision];
+	Vector3 startPointClip[kSubdivision];
+	Vector3 endPointClip[kSubdivision];
 	//从深处到面前按顺序画线
-	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex) {
-		posHorizontalStart[xIndex].y = kGridHalfWidth - xIndex * kGridEvery;
-		posHorizontalStart[xIndex].x = -kGridHalfWidth;
-		posHorizontalEnd[xIndex].y = kGridHalfWidth - xIndex * kGridEvery;
-		posHorizontalEnd[xIndex].x = kGridHalfWidth;
+	for (uint32_t xIndex = 0; xIndex < kSubdivision; ++xIndex) {
+		//使用上面的信息，求出在world坐标里的起点和终点坐标
+		//变换成screen坐标
+		//使用变换后的坐标画线
+		xStart[xIndex] = -kGridHalfWidth - xIndex * kGridEvery;
+		xEnd[xIndex] = xStart[xIndex];
+		zStart[xIndex] = kGridHalfWidth;
+		zEnd[xIndex] = -kGridHalfWidth;
+
+		// 固定 y 坐标为 0，表示 XZ 平面
+		float yCoord = 0.0f;
+		start[xIndex] = { xStart[xIndex], yCoord, zStart[xIndex] };
+		end[xIndex] = { xEnd[xIndex], yCoord, zEnd[xIndex] };
+
+		// 变换到裁剪空间
+		startPointClip[xIndex] = Transform(start[xIndex], worldViewProjectionMatrix);
+		endPointClip[xIndex] = Transform(end[xIndex], worldViewProjectionMatrix);
 
 
-		Vector3 ndcVertexHorizontalStart = Transform(posHorizontalStart[xIndex], worldViewProjectionMatrix);
-		posHorizontalStartScreen[xIndex] = Transform(ndcVertexHorizontalStart, viewPortMatrix);
-		Vector3 ndcVertexHorizontalEnd = Transform(posHorizontalEnd[xIndex], worldViewProjectionMatrix);
-		posHorizontalEndScreen[xIndex] = Transform(ndcVertexHorizontalEnd, viewPortMatrix);
-		Novice::DrawLine((int)posHorizontalStartScreen[xIndex].x, (int)posHorizontalStartScreen[xIndex].y, (int)posHorizontalEndScreen[xIndex].x, (int)posHorizontalEndScreen[xIndex].y, RED);
+		// 变换到视口坐标系
+		startPointViewport[xIndex] = Transform(startPointClip[xIndex], viewPortMatrix);
+		endPointViewport[xIndex] = Transform(endPointClip[xIndex], viewPortMatrix);
+
+		Novice::DrawLine((int)startPointViewport[xIndex].x, (int)startPointViewport[xIndex].z, (int)endPointViewport[xIndex].x, (int)endPointViewport[xIndex].z, RED);
 
 	}
 }
@@ -79,8 +97,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		DrawGrid(projectionMatrix, viewPortMatrix);
 #ifdef _DEBUG
-		MatrixScreenPrintf(-0, kWindowHeight-600, worldViewProjectionMatrix, "worldViewProjectionMatrix");
-		MatrixScreenPrintf(-0, kWindowHeight*6-600, viewPortMatrix, "viewPortMatrix");
+		MatrixScreenPrintf(-0, kWindowHeight - 600, worldViewProjectionMatrix, "worldViewProjectionMatrix");
+		MatrixScreenPrintf(-0, kWindowHeight * 6 - 600, viewPortMatrix, "viewPortMatrix");
 
 #endif 
 
